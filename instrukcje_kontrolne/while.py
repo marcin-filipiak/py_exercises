1c927ce75b32fe9369793056641dc9cadb189ed1
marcin-filipiak <50583432+marcin-filipiak@users.noreply.github.com>
2024-04-05 11:53:24 +0200

    Update while.py

--- instrukcje_kontrolne/while.py
+++ instrukcje_kontrolne/while.py
@@ -1,7 +1,7 @@
 # Inicjalizacja zmiennej
 licznik = 0
 
-# Pętla do puki
+# Pętla while do chwili aż licznik mniejszy od 5
 while licznik < 5:
     print("Wartość licznika:", licznik)
     odpowiedz = input("Czy chcesz kontynuować? (Tak/Nie): ")
